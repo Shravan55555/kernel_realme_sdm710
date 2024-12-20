7b19edc327cdfed9eb12f75b7e1a7841df22cd80
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-12-20 14:25:28 -0800

    susfs: Integrate susfs driver & hooks at v1.5.3
    See https://gitlab.com/simonpunk/susfs4ksu/-/tree/kernel-4.9?ref_type=heads
    
    Note that fs/proc/cmdline.c patch does not apply cleanly with 2nd-stage init commits in LineageOS-based kernel sources for lineage-21 and newer; you will need to manually modify as per this commit.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -984,6 +984,10 @@ struct user_struct {
 #if defined(CONFIG_PERF_EVENTS) || defined(CONFIG_BPF_SYSCALL)
 	atomic_long_t locked_vm;
 #endif
+
+#ifdef CONFIG_KSU_SUSFS
+	u64 android_kabi_reserved2;
+#endif
 };
 
 extern int uids_sysfs_init(void);
@@ -2228,6 +2232,7 @@ struct task_struct {
 	/* A live task holds one reference. */
 	atomic_t stack_refcount;
 #endif
+
 #ifdef OPLUS_FEATURE_UIFIRST
 	int static_ux;
 	atomic64_t dynamic_ux;
@@ -2242,6 +2247,11 @@ struct task_struct {
 #ifdef CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT
 	struct fuse_package *fpack;
 #endif /* CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT */
+
+#ifdef CONFIG_KSU_SUSFS
+	u64 android_kabi_reserved8;
+#endif
+
 /* CPU-specific state of this task */
 	struct thread_struct thread;
 /*
